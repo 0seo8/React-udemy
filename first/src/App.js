08f767c1e4c92542a7366ca139a4bd4284e0efea
0seo8 <96939562+0seo8@users.noreply.github.com>
2022-08-01 13:09:51 +0900

    폴더구조수정

--- first/src/App.js
+++ first/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
-import AddUser from './Users/AddUser'
-import UserList from './Users/UserList'
+import AddUser from './components/Users/AddUser'
+import UserList from './components/Users/UserList'
 
 function App() {
   const [userList, setUserList] = useState([])
