ee4d947d3d2c86f3d3b80d4a772499353b54d920
0seo8 <96939562+0seo8@users.noreply.github.com>
2022-08-02 16:45:10 +0900

    Input컴포넌트를 이용한 마이그네이션

--- thirdapp/src/components/Login/Login.js
+++ thirdapp/src/components/Login/Login.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect, useReducer, useContext } from 'react'
 import Card from '../UI/Card/Card'
 import classes from './Login.module.css'
 import Button from '../UI/Button/Button'
+import Input from '../Input/Input'
 import AuthContext from '../store/auth-context'
 
 const emailReducer = (state, action) => {
@@ -87,29 +88,26 @@ const Login = (props) => {
   return (
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
-        <div
-          className={`${classes.control} ${
-            emailState.isValid === false ? classes.invalid : ''
-          }`}
-        >
-          <label htmlFor="email">E-Mail</label>
-          <input
-            type="email"
-            id="email"
-            value={emailState.value}
-            onChange={emailChangeHandler}
-            onBlur={validateEmailHandler}
-          />
-        </div>
+        <Input
+          id="email"
+          label="E-Mail"
+          type="email"
+          isValid={emailIsValid}
+          vlaue={emailState.value}
+          onChange={emailChangeHandler}
+          onBlur={validateEmailHandler}
+        />
         <div
           className={`${classes.control} ${
             passwordState.isValid === false ? classes.invalid : ''
           }`}
         >
           <label htmlFor="password">Password</label>
-          <input
+          <Input
+            label="password"
             type="password"
             id="password"
+            isValid={passwordValid}
             value={passwordState.value}
             onChange={passwordChangeHandler}
             onBlur={validatePasswordHandler}
