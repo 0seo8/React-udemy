d658a4cec55c40ff6b7ebcbaf2ad24d698748408
0seo8 <96939562+0seo8@users.noreply.github.com>
2022-08-03 16:13:19 +0900

    아이템 동적 추가 삭제

--- food-app/src/components/Cart/Cart.js
+++ food-app/src/components/Cart/Cart.js
@@ -9,10 +9,12 @@ function Cart(props) {
   const totalAmount = `$ ${cartCtx.totalAmount.toFixed(2)} `
   const hasItems = cartCtx.items.lenth > 0
 
-  const cartItemRemoveHandler = (id) => {}
+  const cartItemRemoveHandler = (id) => {
+    cartCtx.removeItem(id)
+  }
 
   const cartItemAddHandler = (item) => {
-    cartCtx.addItem(item)
+    cartCtx.addItem({ ...item, amount: 1 })
   }
 
   const cartItems = (
