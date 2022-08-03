3b23b18b519a5e172c168de71c76eedd698ef2cb
0seo8 <96939562+0seo8@users.noreply.github.com>
2022-08-03 17:29:12 +0900

    로딩처리

--- httprequest/src/App.js
+++ httprequest/src/App.js
@@ -5,9 +5,12 @@ import './App.css'
 
 function App() {
   const [movies, setMovies] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+
   async function fetchMoviesHandler() {
+    setIsLoading(true)
     const response = await fetch('https://swapi.dev/api/films/')
-    const data = response.json()
+    const data = await response.json()
     const transformMovies = data.results.map((movieData) => {
       return {
         id: movieData.episode_id,
@@ -17,6 +20,7 @@ function App() {
       }
     })
     setMovies(transformMovies)
+    setIsLoading(false)
   }
 
   return (
@@ -25,7 +29,9 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && <p>Found no movies.</p>}
+        {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
   )
