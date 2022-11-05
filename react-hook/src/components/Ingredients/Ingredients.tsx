efad6e8f1db73ae0bea231ecce115f883e66da59
0seo8 <96939562+0seo8@users.noreply.github.com>
2022-11-05 20:59:19 +0900

    feat: firebase와 연결

--- react-hook/src/components/Ingredients/Ingredients.tsx
+++ react-hook/src/components/Ingredients/Ingredients.tsx
@@ -12,15 +12,20 @@ interface ingredient {
 
 const Ingredients = () => {
   const [userIngredients, setUserIngredients] = useState<ingredient[]>([])
+
   const addIngredientHandler = (ingredient: ingredient) => {
-    setUserIngredients(prevIngredients => [
-      ...prevIngredients, 
-      {
-        id: Math.random().toString(),
-        ...ingredient
-      }
-    ]
-  )}
+    fetch('https://react-hooks-update-9a96f-default-rtdb.firebaseio.com/ingredients.json', {
+      method:'POST',
+      body: JSON.stringify(ingredient),
+      headers: { 'Content-Type': 'application/json' }
+    }).then(response => {
+      return response.json()
+    }).then(responseData => {
+      setUserIngredients(prevIngredients => [
+        ...prevIngredients, 
+        { id: responseData.name, ...ingredient}
+      ])
+    })}
   return (
     <div className="App">
       <IngredientForm onAddIngredient={addIngredientHandler}/>
