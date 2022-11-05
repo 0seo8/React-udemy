3db2d6ae21266b8a0a95aa8a511a910bfe50e5cf
0seo8 <96939562+0seo8@users.noreply.github.com>
2022-11-05 22:01:12 +0900

    feat: useEffect를 이용해 데이터 가져오기

--- react-hook/src/components/Ingredients/Ingredients.tsx
+++ react-hook/src/components/Ingredients/Ingredients.tsx
@@ -1,18 +1,37 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react'
 // import axios from 'axios'
 
-import IngredientForm from './IngredientForm';
-import IngredientList from './IngredientList';
-import Search from './Search';
+import IngredientForm from './IngredientForm'
+import IngredientList from './IngredientList'
+import Search from './Search'
 
 interface ingredient {
-  id?: string,
-  title: string,
+  id?: string
+  title: string
   amount: string
 }
 
 const Ingredients = () => {
   const [userIngredients, setUserIngredients] = useState<ingredient[]>([])
+
+  //모든 컴포넌트의 렌더잉이 끝난 후 실행됩니다.렌더링이 될때마다.
+  useEffect(() => {
+    fetch(
+      'https://react-hooks-update-9a96f-default-rtdb.firebaseio.com/ingredients.json',
+    )
+      .then((response) => response.json())
+      .then((responseData) => {
+        const loadedIngredients = []
+        for (const key in responseData) {
+          loadedIngredients.push({
+            id: key,
+            title: responseData[key].title,
+            amount: responseData[key].amount,
+          })
+        }
+        setUserIngredients(loadedIngredients)
+      })
+  }, [])
 
   const addIngredientHandler = (ingredient: ingredient) => {
     // axios('https://react-hooks-update-9a96f-default-rtdb.firebaseio.com/ingredients.json', {
@@ -21,34 +40,39 @@ const Ingredients = () => {
     //   data: JSON.stringify(ingredient)
     // }).then(response => {
     //   setUserIngredients(prevIngredients => [
-    //     ...prevIngredients, 
+    //     ...prevIngredients,
     //     { id: response.data.name, ...ingredient}
     //   ])
     // })
 
-    fetch('https://react-hooks-update-9a96f-default-rtdb.firebaseio.com/ingredients.json', {
-      method:'POST',
-      body: JSON.stringify(ingredient),
-      headers: { 'Content-Type': 'application/json' }
-    }).then(response => {
-      return response.json()
-    }).then(responseData => {
-      setUserIngredients(prevIngredients => [
-        ...prevIngredients, 
-        { id: responseData.name, ...ingredient}
-      ])
-    })
+    fetch(
+      'https://react-hooks-update-9a96f-default-rtdb.firebaseio.com/ingredients.json',
+      {
+        method: 'POST',
+        body: JSON.stringify(ingredient),
+        headers: { 'Content-Type': 'application/json' },
+      },
+    )
+      .then((response) => {
+        return response.json()
+      })
+      .then((responseData) => {
+        setUserIngredients((prevIngredients) => [
+          ...prevIngredients,
+          { id: responseData.name, ...ingredient },
+        ])
+      })
   }
   return (
     <div className="App">
-      <IngredientForm onAddIngredient={addIngredientHandler}/>
+      <IngredientForm onAddIngredient={addIngredientHandler} />
 
       <section>
         <Search />
-        <IngredientList ingredients={userIngredients} onRemoveItem={() =>{}}/>
+        <IngredientList ingredients={userIngredients} onRemoveItem={() => {}} />
       </section>
     </div>
-  );
+  )
 }
 
-export default Ingredients;
+export default Ingredients
