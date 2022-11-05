af8b2cddd4f3656ca705dcebe97e157aa838e16f
0seo8 <96939562+0seo8@users.noreply.github.com>
2022-11-05 21:06:57 +0900

    feat: axios로 리팩

--- react-hook/src/components/Ingredients/Ingredients.tsx
+++ react-hook/src/components/Ingredients/Ingredients.tsx
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+// import axios from 'axios'
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
@@ -14,6 +15,17 @@ const Ingredients = () => {
   const [userIngredients, setUserIngredients] = useState<ingredient[]>([])
 
   const addIngredientHandler = (ingredient: ingredient) => {
+    // axios('https://react-hooks-update-9a96f-default-rtdb.firebaseio.com/ingredients.json', {
+    //   method:'post',
+    //   headers: {'Content-Type': 'application/json'},
+    //   data: JSON.stringify(ingredient)
+    // }).then(response => {
+    //   setUserIngredients(prevIngredients => [
+    //     ...prevIngredients, 
+    //     { id: response.data.name, ...ingredient}
+    //   ])
+    // })
+
     fetch('https://react-hooks-update-9a96f-default-rtdb.firebaseio.com/ingredients.json', {
       method:'POST',
       body: JSON.stringify(ingredient),
@@ -25,7 +37,8 @@ const Ingredients = () => {
         ...prevIngredients, 
         { id: responseData.name, ...ingredient}
       ])
-    })}
+    })
+  }
   return (
     <div className="App">
       <IngredientForm onAddIngredient={addIngredientHandler}/>
