f4b7a387972db663bc8327e5745854aa2642da6f
0seo8 <96939562+0seo8@users.noreply.github.com>
2022-11-06 22:21:03 +0900

    fix: search 무한루프

--- react-hook/src/components/Ingredients/Ingredients.tsx
+++ react-hook/src/components/Ingredients/Ingredients.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 // import axios from 'axios'
 
 import IngredientForm from './IngredientForm'
@@ -14,28 +14,12 @@ interface ingredient {
 const Ingredients = () => {
   const [userIngredients, setUserIngredients] = useState<ingredient[]>([])
 
-  //모든 컴포넌트의 렌더잉이 끝난 후 실행됩니다.렌더링이 될때마다.
-  useEffect(() => {
-    fetch(
-      'https://react-hooks-update-9a96f-default-rtdb.firebaseio.com/ingredients.json',
-    )
-      .then((response) => response.json())
-      .then((responseData) => {
-        const loadedIngredients = []
-        for (const key in responseData) {
-          loadedIngredients.push({
-            id: key,
-            title: responseData[key].title,
-            amount: responseData[key].amount,
-          })
-        }
-        setUserIngredients(loadedIngredients)
-      })
-  }, [])
-
-  const filteredIngredientsHandler = (filteredIngredients: ingredient[]) => {
-    setUserIngredients(filteredIngredients)
-  }
+  const filteredIngredientsHandler = useCallback(
+    (filteredIngredients: ingredient[]) => {
+      setUserIngredients(filteredIngredients)
+    },
+    [],
+  )
 
   const addIngredientHandler = (ingredient: ingredient) => {
     fetch(
