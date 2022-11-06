9c30dd85651cb556367febaf09dcb980c4dc5b00
0seo8 <96939562+0seo8@users.noreply.github.com>
2022-11-06 22:31:04 +0900

    useEffect을 이용한 search

--- react-hook/src/components/Ingredients/Search.tsx
+++ react-hook/src/components/Ingredients/Search.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 
 import Card from '../UI/Card'
 import './Search.css'
@@ -12,29 +12,37 @@ const Search: React.FC<{
   onLoadIngredients: (loadedIngredients: ingredient[]) => void
 }> = React.memo(({ onLoadIngredients }) => {
   const [enteredFilter, setEnteredFilter] = useState('')
+  const inputRef = useRef<HTMLInputElement>(null)
 
   useEffect(() => {
-    const query =
-      enteredFilter.length === 0
-        ? ''
-        : `?orderBy="title"&equalTo="${enteredFilter}"`
-    fetch(
-      'https://react-hooks-update-9a96f-default-rtdb.firebaseio.com/ingredients.json' +
-        query,
-    )
-      .then((response) => response.json())
-      .then((responseData) => {
-        const loadedIngredients = []
-        for (const key in responseData) {
-          loadedIngredients.push({
-            id: key,
-            title: responseData[key].title,
-            amount: responseData[key].amount,
+    const timer = setTimeout(() => {
+      if (enteredFilter === inputRef.current?.value) {
+        const query =
+          enteredFilter.length === 0
+            ? ''
+            : `?orderBy="title"&equalTo="${enteredFilter}"`
+        fetch(
+          'https://react-hooks-update-9a96f-default-rtdb.firebaseio.com/ingredients.json' +
+            query,
+        )
+          .then((response) => response.json())
+          .then((responseData) => {
+            const loadedIngredients = []
+            for (const key in responseData) {
+              loadedIngredients.push({
+                id: key,
+                title: responseData[key].title,
+                amount: responseData[key].amount,
+              })
+            }
+            onLoadIngredients(loadedIngredients)
           })
-        }
-        onLoadIngredients(loadedIngredients)
-      })
-  }, [enteredFilter, onLoadIngredients])
+      }
+    }, 500)
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [enteredFilter, onLoadIngredients, inputRef])
 
   return (
     <section className="search">
@@ -42,6 +50,7 @@ const Search: React.FC<{
         <div className="search-input">
           <label>Filter by Title</label>
           <input
+            ref={inputRef}
             type="text"
             value={enteredFilter}
             onChange={(event) => setEnteredFilter(event.target.value)}
